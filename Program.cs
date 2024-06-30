fb883c36a3e91a7b81005ce691ba848281f31016
Jchillah <jchillah@gmail.com>
2024-06-30 15:26:18 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.WriteLine("what is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}");
+Console.Write($"{Environment.NewLine}press any key to exit...");
+Console.ReadKey(true);
